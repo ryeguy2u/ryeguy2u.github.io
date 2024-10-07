dab42b9f5b2f3a9162f67cd1392d8f2d491b27b9
ryeguy2u <ryeguy2u@gmail.com>
2024-10-07 14:34:53 +0000

    class thing

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -13,7 +13,7 @@ $(function () {
       $(document).on("keyup", handleKeyUp);
       firstTimeSetup = false;
       //start game
-      setInterval(main, 1000 / frameRate);
+      setInterval(main, 950 / frameRate);
     }
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); //top
@@ -59,6 +59,34 @@ $(function () {
 
 
 
+    createPlatform(200,0, 25,580)
+    createPlatform(220,555, 300,25)
+    createPlatform(0,250, 150,25)
+    createPlatform(40,370, 160.5,25)
+    createPlatform(200,680, 25,100)
+    createPlatform(0,680, 200,25)
+    createCollectable('database',300,600,1,1)
+    createPlatform(415,680,25,100)
+    createCannon('right',760,1500,30,30)
+    createPlatform(220,555, 900,25)
+    createPlatform(1250,555, 300,25)
+    createPlatform(1090,680, 200,400)
+    createPlatform(1200,420, 150,25)
+    createPlatform(1250,300, 150,25)
+    createPlatform(1100,210, 25,370)
+    createCannon('bottom',1230,3000,30,30)
+    createPlatform(975,280, 150,25)
+    createPlatform(745,300, 150,25)
+    createPlatform(600,430, 150,25)
+    createCannon('top',1210,3000,30,30)
+    createPlatform(1175,210, 25,370)
+    createCollectable('database',1130,380,0,0)
+    createPlatform(400,200, 150,25)
+    createCollectable('database',650,60,0,0)
+    createCollectable('database',1030,500,0,0)
+    createPlatform(1000,420, 0.1,160)
+    createPlatform(1320,100,150,0.01)
+    createCollectable('database',1340,50,0.1,1)
     
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
