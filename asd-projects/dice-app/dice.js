72666f8ac7c080d4917eb4c3f8303468b3b7fd94
ryeguy2u <ryeguy2u@gmail.com>
2025-09-11 14:37:42 +0000

    dice completed

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,64 @@
 $(document).ready(function () {
   // Your code goes here
-
+$("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", 50)
+  .css("left", 50)
+  .appendTo("#die");
 });
+$("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", 50)
+  .css("left", 50)
+  .appendTo("#die");
+  function makeDot(top, left, elementID){
+    $("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", top)
+  .css("left", left)
+  .appendTo(elementID);}
+  function rollDie(){
+    $("#die").empty();
+    var randomNum = Math.ceil(Math.random() * 6);
+console.log(randomNum);
+if (randomNum === 1) {
+  makeDot(50, 50, "#die"); // middle middle
+} else if (randomNum === 2) {
+  makeDot(25, 25, "#die"); // top left
+  makeDot(75, 75, "#die"); // bottom right
+} else if (randomNum === 3) {
+  makeDot(25, 25, "#die"); // top left
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(50, 50, "#die"); // middle middle
+} else if (randomNum === 4) {
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(25, 25, "#die"); // top left
+  makeDot(25, 75, "#die"); // bottom left
+  makeDot(75, 25, "#die"); // top right
+} else if (randomNum === 5) {
+  makeDot(50, 50, "#die"); // middle middle
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(25, 25, "#die"); // top left
+  makeDot(25, 75, "#die"); // bottom left
+  makeDot(75, 25, "#die"); // top right
+} else if (randomNum === 6) {
+  makeDot(50, 75, "#die"); // middle right
+  makeDot(50, 25, "#die"); // middle left
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(25, 25, "#die"); // top left
+  makeDot(25, 75, "#die"); // bottom left
+  makeDot(75, 25, "#die"); // top right
+}
+    console.log("clicked");
+  }
+
+  $("#die").on("click", rollDie);
